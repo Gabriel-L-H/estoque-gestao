7c79146d8e9604f832761a53629216520c2ef060
Gabriel Leme Horacio <lemehoraciogabriel@gmail.com>
2025-07-06 19:06:09 -0300

    Moving a config

--- src/main/java/br/com/estoquegestao/gabriel/service/JwtUtil.java
+++ src/main/java/br/com/estoquegestao/gabriel/service/JwtUtil.java
@@ -1,20 +1,23 @@
 package br.com.estoquegestao.gabriel.service;
 
-import br.com.estoquegestao.gabriel.config.KeyJWT;
 import br.com.estoquegestao.gabriel.model.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
 public class JwtUtil {
+    private static final Dotenv dotenv = Dotenv.configure()
+                                        .ignoreIfMissing()
+                                        .load();
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
-    private static final Algorithm secretKey = Algorithm.HMAC256(KeyJWT.getJwtSecretKey());
+    private static final Algorithm secretKey = Algorithm.HMAC256(dotenv.get("JWT_SECRET_KEY"));
     private static final JWTVerifier verifier = JWT.require(secretKey)
                                                 .withIssuer("https://localhost:3306/estoque_vendas")
                                                 .build();
